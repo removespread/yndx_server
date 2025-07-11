ff13924bd22d4606fe71592d4528880c46917e18
Victor S. <removespread@proton.me>
2025-07-12 00:17:28 +0300

    added new fixtures in index

--- fe-tests/fixtures/index.ts
+++ fe-tests/fixtures/index.ts
@@ -1,10 +1,14 @@
 import { ShokMainPage } from "./ShokMainPage";
 import { ShokLoginPage } from "./ShokLoginPage";
 import { test as base } from "@playwright/test";
+import { ShokRenamePage } from "./ShokRenamePage";
+import { ShokProfilePage } from "./ShokProfilePage";
 
 type MyFixtures = {
     mainPage: ShokMainPage;
     loginPage: ShokLoginPage;
+    renamePage: ShokRenamePage;
+    profilePage: ShokProfilePage;
 }
 
 export const test = base.extend<MyFixtures>({
@@ -17,5 +21,16 @@ export const test = base.extend<MyFixtures>({
         const loginPage = new ShokLoginPage(page);
         await loginPage.open();
         await use(loginPage);
+    },
+    renamePage: async ({ page }, use) => {
+        const renamePage = new ShokRenamePage(page);
+        await renamePage.open();
+        await use(renamePage);
+    },
+    profilePage: async ({ page }, use) => {
+        const profilePage = new ShokProfilePage(page);
+        await profilePage.open();
+        await use(profilePage);
     }
+    
 });
